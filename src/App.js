3914bd424ef39e0bc7ea018e9d84cc09276e5c9f
xi-khan <xihan123@gmail.com>
2020-10-14 10:55:32 +0500

    Added card component using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useFetchJobs from './useFetchjobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
 
 const App = () => {
-  const { jobs, loading, error } = useFetchJobs();
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
     <Container>
       {loading && <h1>loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
